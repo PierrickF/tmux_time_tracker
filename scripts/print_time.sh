5c09b063df2993c6fbe9b0c770bcf6e45846c110
Pierrick Foulquier <pierrickfoulquier@gmail.com>
2022-03-05 14:58:45 +0100

    Save time elapsed in a file

--- scripts/print_time.sh
+++ scripts/print_time.sh
@@ -8,11 +8,12 @@ main() {
 
   SECONDS=0
 
-  while [ $SECONDS -lt 5 ]
+  while :
     do
       printf '%02d:%02d:%02d\n'   $((SECONDS/3600)) \
                                   $((SECONDS%3600/60)) \
                                   $((SECONDS%60))
+      echo "$SECONDS" > "$CURRENT_DIR/counter"
       sleep 1
   done
 }
