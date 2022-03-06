9efb3bcf07fd7eee5c19268bf8996c5d3457f952
Pierrick Foulquier <pierrickfoulquier@gmail.com>
2022-03-06 12:54:02 +0100

    Timer resets when counter file is removed

--- scripts/timer.sh
+++ scripts/timer.sh
@@ -2,13 +2,14 @@
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-#source "$CURRENT_DIR/helpers.sh"
-
 main() {
   while :
   do
-    if [ -f "$CURRENT_DIR/counter" ]; then
+    if [ -f "$CURRENT_DIR/counter" ]
+    then
       SECONDS=$(<"$CURRENT_DIR/counter")
+    else
+      SECONDS=0
     fi
     printf '%02d:%02d:%02d\n'   $((SECONDS/3600)) \
                                 $((SECONDS%3600/60)) \
